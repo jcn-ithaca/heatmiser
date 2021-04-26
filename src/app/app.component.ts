e3df42f58ef706cabc7572b14d36737e422e7818
Joe Nolan <jnolan@creativeradicals.com>
2021-04-26 08:52:44 -0400

    pad lat/lng to 3 decimal places;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {latLng, Map, tileLayer} from 'leaflet';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {latLng, tileLayer} from 'leaflet';
 import * as L from 'leaflet';
 import 'leaflet-heatmap';
-import 'leaflet.heat/dist/leaflet-heat';
 import tracks_1 from 'testdata/a1.json';
 import tracks_2 from 'testdata/a2.json';
 import tracks_3 from 'testdata/a3.json';
@@ -30,12 +29,17 @@ import tracks_23 from 'testdata/a23.json';
 type HeatmapOverlayConstructor = new(cfg: any) => any;
 declare var HeatmapOverlay: HeatmapOverlayConstructor;
 
+interface OioDatum {
+  count: number;
+  bin: {latitude: number, longitude: number};
+}
+
 interface HmDatum {
   lat: number;
   lng: number;
   count: number;
-  binID: string;
-  numDevices: number;
+  // binID: string;
+  // numDevices: number;
 }
 
 @Component({
@@ -47,97 +51,99 @@ export class AppComponent implements OnInit {
   leafletOpts;
   layersControl;
   heatLayer: any;   // leaflet-heatmap
-  dotHeatLayer: any;   // leaflet.heat
 
   heatLayers = [this.heatLayer];
-  dataArrays = [
+  dataArrays: any[] = [
     tracks_1, tracks_2, tracks_3, tracks_4, tracks_5, tracks_6,
     tracks_7, tracks_8, tracks_9, tracks_10, tracks_11, tracks_12,
     tracks_13, tracks_14, tracks_15, tracks_16, tracks_17, tracks_18,
     tracks_19, tracks_20, tracks_21, tracks_22, tracks_23
     ];
   leaf = L as any;
-  combinedData: HmDatum[] = [];
-  mergedData: HmDatum[] = [];
-  filteredData: HmDatum[] = [];
+  // @ts-ignore
+  combinedData: Map<string, HmDatum> = new Map<string, HmDatum>();
+  // mergedData: HmDatum[] = [];
+  // filteredData: HmDatum[] = [];
+  a: Set<HmDatum>;
   radMin = 20;
   radMax = 60;
   radStep = 5;
   dataMax = 0;
   muteLowActivity = false;
-  muteMinimum = 5;
+  // muteMinimum = 5;
   highlightConvergence = false;
-  highlightFactor = 10;
+  // highlightFactor = 10;
   opacityLevel = 1;
   opacityLabels = ['Low', 'Medium', 'High'];
   hotspotIndex = 0;
   hotspotOptions = ['Based on local maximum', 'Based on global maximum'];
+  zoomLevel = 0;
 
-  gradMulti = {
+  gradMulti: object = {
     0.0 : '#0000FF',
     0.25 : '#adff2f',
     0.5 : '#FFFF00',
     0.75 : '#feb24c',
     1.0 : '#bd0026',
   };
-  gradYOR1 = {
+  gradYOR1: object = {
     0.0 : '#eed976',
     0.4 : '#feb24c',
     0.6 : '#fd8d3c',
     1.0 : '#bd0026',
   };
-  gradYOR2 = {
+  gradYOR2: object = {
     0.0 : '#fed976',
     0.9 : '#fd8d3c',
     1.0 : '#e31a1c',
   };
-  gradYOR3 = {
+  gradYOR3: object = {
     0.0 : '#fed976',
     0.1 : '#fd8d3c',
     1.0 : '#e31a1c',
   };
-  gradYOR4 = {
+  gradYOR4: object = {
     0.0 : '#eed976',
     0.4 : '#feb24c',
     0.6 : '#fd8d3c',
     1.0 : '#bd0026',
   };
-  gradBP = {
+  gradBP: object = {
     0.0 : '#b3cde3',
     0.5 : '#8856a7',
     1.0 : '#810f7c',
   };
-  gradO = {
+  gradO: object = {
     0.0 : '#fdbe85',
     0.3 : '#fd8d3c',
     0.7 : '#e6550d',
     1.0 : '#a63603',
   };
-  gradR = {
+  gradR: object = {
     0.0 : '#fcae91',
     0.3 : '#fb6a4a',
     0.7 : '#de2d26',
     1.0 : '#a50f15',
   };
-  gradB = {
+  gradB: object = {
     0.0 : '#bdd7e7',
     0.3 : '#6baed6',
     0.7 : '#3182bd',
     1.0 : '#08519c',
   };
-  gradP = {
+  gradP: object = {
     0.0 : '#cbc9e2',
     0.3 : '#9e9ac8',
     0.7 : '#756bb1',
     1.0 : '#54278f',
   };
-  gradG = {
+  gradG: object = {
     0.0 : '#bae4b3',
     0.3 : '#74c476',
     0.7 : '#31a354',
     1.0 : '#006d2c',
   };
-  whichGradient = this.gradYOR1;
+  whichGradient: object = this.gradMulti;
   gradients = [
     {label: 'Yellow-Orange-Red #1', gradient: this.gradYOR1},
     {label: 'Yellow-Orange-Red #2', gradient: this.gradYOR2},
@@ -174,7 +180,7 @@ export class AppComponent implements OnInit {
     valueField: 'count'
   };
 
-  constructor() {
+  constructor(private cdr: ChangeDetectorRef) {
   }
 
   hotspotChanged() {
@@ -207,11 +213,32 @@ export class AppComponent implements OnInit {
       detectRetina: true,
       attribution: '&amp;copy; &lt;a href="https://www.openstreetmap.org/copyright"&gt;OpenStreetMap&lt;/a&gt; contributors'
     });
+    const baseMap3 = tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      detectRetina: true,
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+    const baseMap4 = L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+      maxZoom: 20,
+      attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+    const baseMap5 = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+      maxZoom: 20,
+      attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    });
+    const baseMap6 = L.tileLayer('https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey={apikey}', {
+      attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      // apikey: '<your apikey>',
+      maxZoom: 22
+    });
 
     this.layersControl = {
       baseLayers: {
         'Wikimedia Map' : baseMap1,
-        'Stadia Map': baseMap2
+        'Stadia Map' : baseMap2,
+        'OpenTopo Map' : baseMap3,
+        'CyclOSM Map' : baseMap4,
+        'StadiaDark Map' : baseMap5,
+        'ThunderForest OpenCycle' : baseMap6
       },
       overlays: {}
     };
@@ -223,24 +250,19 @@ export class AppComponent implements OnInit {
       center: latLng([40, -75])
     };
 
-    // Find overall maximum count value (our JSON data is sorted by count)
-    // for (const arr of this.dataArrays) {
-    //   if (arr[0].count > this.dataMax) {
-    //     this.dataMax = arr[0].count;
-    //   }
-    // }
-
+    this.zoomLevel = 7;
     this.setDefaultConfig(1);
-    this.addAllHeatmaps();
+    this.combineData();
+    this.addLeafletHeatmap();
   }
 
   updateDataMax(): void {
     this.dataMax = 0;
-    // const whichData = this.filteredData ? this.filteredData : this.mergedData;
-    const whichData = this.mergedData;  // allow boosted cells to exceed dataMax?
+    const whichData = this.combinedData;  // allow boosted cells to exceed dataMax?
 
     if (whichData) {
-      for (const d of whichData) {
+      // @ts-ignore
+      for (const d of whichData.values()) {
         if (d.count > this.dataMax) {
           this.dataMax = d.count;
         }
@@ -251,7 +273,7 @@ export class AppComponent implements OnInit {
   setDefaultConfig(n: number): void {
     switch (n) {
       case 1:
-        this.whichGradient = this.gradYOR1;
+        this.whichGradient = this.gradMulti;
         this.lhConfig.useLocalExtrema = true;
         this.lhConfig.scaleRadius = false;
         this.lhConfig.radius = 35;
@@ -290,145 +312,112 @@ export class AppComponent implements OnInit {
     this.lhConfig.gradient = this.whichGradient;
 
     if (this.heatLayer) {
+      const startTime = performance.now();
       // Hack to redraw heatmap (https://github.com/pa7/heatmap.js/issues/290)
       this.heatLayer._heatmap.configure(this.lhConfig);
       this.heatLayer._reset();
+      const endTime = performance.now();
+      console.log(`>>> Heatmap rendering took: ${Math.round(endTime - startTime)} ms;`);
     }
-
-    // Update leaflet.heat layer
-    // if (this.dotHeatLayer) {
-    //   const opts = {
-    //     radius: this.lhConfig.radius,
-    //     minOpacity: this.lhConfig.minOpacity,
-    //     max: this.dataMax,
-    //     gradient: this.whichGradient
-    //   };
-    //   this.dotHeatLayer.setOptions(opts);
-    // }
   }
-
-  resetDataAndSettings() {
-    this.addLeafletHeatmap();
-    this.settingsChanged();
-  }
-
-  addDotHeat(): void {
-    const opts1 = {
-      radius: 35,
-      minOpacity: .4,
-      max: 38000,
-      gradient: this.gradYOR1
-    };
-    const latLngArr = this.mergedData.map(d => ({lat: d.lat, lng: d.lng, alt: d.count}));
-    this.dotHeatLayer = this.leaf.heatLayer(latLngArr, opts1);
-    this.layersControl.overlays[`Dot Heat`] = this.dotHeatLayer;
-  }
-
-  // addWebGlHeatmap(): void {
-  //   const opts = {
-  //     size: 30,
-  //     units: 'px',
-  //     alphaRange: 0.5, // ??
-  //     max: 35000,
-  //     autoresize: true
-  //   };
-  //   const wglData = this.mergedData.map(d => ( [d.lat, d.lng, d.count] ));
-  //   const layer = new this.leaf.WebGLHeatMap(opts);
-  //   layer.setData(wglData);
-  //   this.layersControl.overlays[`WebGL Heatmap`] = layer;
-  // }
 
   addLeafletHeatmap(): void {
     this.heatLayer = new HeatmapOverlay(this.lhConfig);
-    const layerData = {min: 0, max: this.dataMax, data: this.mergedData};
+    const combinedArr = Array.from(this.combinedData.values());
+    const layerData = {min: 0, max: this.dataMax, data: combinedArr};
     this.heatLayer.setData(layerData);
     this.layersControl.overlays[`LH Heatmap`] = this.heatLayer;
     this.heatLayers = [this.heatLayer];
   }
 
-  refilterData() {
-    this.combineData();
-    this.filteredData = JSON.parse(JSON.stringify(this.mergedData));
-    let boosted = 0;
+  addHeatMapData(locMap: Map<string, HmDatum>, oioData: OioDatum[]) {
+    for (const d of oioData) {
+      const id = `${d.bin.latitude.toFixed(3)}:${d.bin.longitude.toFixed(3)}`;
+      const hmDatum: HmDatum = {lat: d.bin.latitude, lng: d.bin.longitude, count: d.count};
 
-    if (this.muteLowActivity) {
-      this.filteredData = this.filteredData.filter(d => d.count >= this.muteMinimum);
-      console.log(`>>> Muted bins: ${this.filteredData.length};`);
-    }
-
-    if (this.highlightConvergence) {
-      for (const bd of this.filteredData) {
-        if (bd.numDevices > 1) {
-          bd.count = bd.count * (bd.numDevices * this.highlightFactor);
-          boosted++;
-          if (bd.numDevices === 3) {
-            console.log(`>>> Found triple cell at : ${bd.lat} : ${bd.lng} with boosted count = ${bd.count};`);
-          }
-        }
+      if (locMap.has[id]) {
+        const val = locMap.get(id);
+        val.count += hmDatum.count;
+        locMap.set(id, val);
+      } else {
+        locMap.set(id, hmDatum);
       }
     }
-    const doubles = this.filteredData.filter(d => d.numDevices === 2);
-    const triples = this.filteredData.filter(d => d.numDevices === 3);
-    console.log(`>>> Double cells: ${doubles.length};`);
-    console.log(`>>> Triple cells: ${triples.length};`);
-    console.log(`>>> Boosted ${boosted} multi-device cells;`);
-
-    this.updateDataMax();
-    const layerData = {min: 0, max: this.dataMax, data: this.filteredData};
-    console.log(`>>> Setting data with dataMax of ${this.dataMax};`);
-    this.heatLayer.setData(layerData);
   }
 
   combineData(): void {
-    this.combinedData = [];
-    this.mergedData = [];
-    this.filteredData = [];
+    this.combinedData.clear();
+    const startTime = performance.now();
+    let totalPts = 0;
 
     for (const arr of this.dataArrays) {
-      // tslint:disable-next-line:max-line-length
-      const latLngArr: HmDatum[] = arr.map(d => ({lat: d.bin.latitude, lng: d.bin.longitude, count: d.count, binID: `${d.bin.latitude}:${d.bin.longitude}`, numDevices: 1}));
-      // tslint:disable-next-line:max-line-length
-      // const latLngArr: HmDatum[] = arr.map(d => ({lat: d.track.location.latitude, lng: d.track.location.longitude, count: 1, binID: `${d.track.location.latitude}:${d.track.location.longitude}`, numDevices: 1}));
-      this.combinedData = this.combinedData.concat(latLngArr);
+      this.addHeatMapData(this.combinedData, arr);
+      totalPts += arr.length;
     }
-    // Sort data so points with same bin (grid cell) are guaranteed to be adjacent in the combined array
-    this.combinedData.sort((a, b) => a.binID.localeCompare(b.binID) );
-    console.log(`>>> Combined bins: ${this.combinedData.length };`);
+    const endTime = performance.now();
 
-    // Merge points in same bin
-    while (this.combinedData.length > 0) {
-      if (this.mergedData.length) {
-        const m = this.mergedData[0];
-        const c = this.combinedData[0];
-        if (c.lat === m.lat && c.lng === m.lng) {  // same bin
-          m.count += c.count;
-          // m.numDevices++;
-          this.combinedData.shift();
-        } else {
-          this.mergedData.unshift(this.combinedData.shift());
-        }
-      } else {
-        this.mergedData.unshift(this.combinedData.shift());
-      }
-    }
-    console.log(`>>> Merged bins: ${this.mergedData.length};`);
+    console.log(`>>> Total locations: ${totalPts};`);
+    console.log(`>>> Combined locations: ${this.combinedData.size};`);
+    console.log(`>>> Combining took: ${Math.round(endTime - startTime)} ms;`);
     this.updateDataMax();
+    console.log(`>>> Maximum count: ${this.dataMax};`);
   }
 
-  addAllHeatmaps() {
-    this.combineData();
-    this.addLeafletHeatmap();
-    this.addDotHeat();
-    // this.addWebGlHeatmap();
-  }
-
-  onMapReady(map: Map) {
+  onMapReady(map: any) {
+    console.log('>>> Map is ready');
+    map.on('zoomend', ev => this.onZoomEnd(map, ev) );
     (map as any)._onResize();
     setTimeout(() => this.settingsChanged(), 200);
-    // map.fitBounds([
-    //   [39, -76],
-    //   [41, -74]
-    // ]);
   }
 
+  onZoomEnd(map: any, ev: any) {
+    this.zoomLevel = map.getZoom();
+    console.log(`>>> Zoom is now: ${this.zoomLevel};`);
+    const zoomFactor = (this.zoomLevel * 11) / 17;
+    this.lhConfig.radius = Math.round((zoomFactor * zoomFactor) + 5);
+    this.settingsChanged();
+    this.cdr.detectChanges();
+  }
 }
+
+
+// resetDataAndSettings() {
+//   this.addLeafletHeatmap();
+//   this.settingsChanged();
+// }
+
+// refilterData() {
+//   this.combineData();
+//   this.filteredData = JSON.parse(JSON.stringify(this.mergedData));
+//   let boosted = 0;
+//
+//   if (this.muteLowActivity) {
+//     this.filteredData = this.filteredData.filter(d => d.count >= this.muteMinimum);
+//     console.log(`>>> Muted bins: ${this.filteredData.length};`);
+//   }
+//
+//   if (this.highlightConvergence) {
+//     for (const bd of this.filteredData) {
+//       if (bd.numDevices > 1) {
+//         bd.count = bd.count * (bd.numDevices * this.highlightFactor);
+//         boosted++;
+//         if (bd.numDevices === 3) {
+//           console.log(`>>> Found triple cell at : ${bd.lat} : ${bd.lng} with boosted count = ${bd.count};`);
+//         }
+//       }
+//     }
+//   }
+//   const doubles = this.filteredData.filter(d => d.numDevices === 2);
+//   const triples = this.filteredData.filter(d => d.numDevices === 3);
+//   console.log(`>>> Double cells: ${doubles.length};`);
+//   console.log(`>>> Triple cells: ${triples.length};`);
+//   console.log(`>>> Boosted ${boosted} multi-device cells;`);
+//
+//   this.updateDataMax();
+//   const layerData = {min: 0, max: this.dataMax, data: this.filteredData};
+//   console.log(`>>> Setting data with dataMax of ${this.dataMax};`);
+//   this.heatLayer.setData(layerData);
+// }
+
+// tslint:disable-next-line:max-line-length
+// const locations: HmDatum[] = arr.map(d => ({lat: d.bin.latitude, lng: d.bin.longitude, count: d.count, binID: `${d.bin.latitude}:${d.bin.longitude}`}));
